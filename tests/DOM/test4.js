b4f174a279822cf32b19661139c9265ae3677b8d
sobhan <sobhan.saf79@gmail.com>
2021-05-03 12:25:35 +0430

    added some tests for functions

---
+++ tests/DOM/test4.js
@@ -0,0 +1,19 @@
+function t (a, b, c)
+{
+    if (a > 10) {
+        var a = 10;
+    } else {
+        var b = 30;
+    }
+}
+
+var a = function () {
+    if (10 > 20) {
+        if (true) {
+
+        } else
+        {
+
+        }
+    }
+}
