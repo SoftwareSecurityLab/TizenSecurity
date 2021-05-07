a140f7bc4140dc3d57eab46ff27c19c940a2780c
sobhan <sobhan.saf79@gmail.com>
2021-05-07 19:40:35 +0430

    input file now are prettified before reading

--- TizEx/TizEx.py
+++ TizEx/TizEx.py
@@ -3,6 +3,7 @@ import subprocess
 from variable_declaration import *
 from functions import *
 from shared import *
+from html5print import JSBeautifier 
 
 # TODO:
 # 1. fixing the method of saving JS varirables
@@ -11,6 +12,9 @@ from shared import *
 # 4. handling fetch and ajax and ... functions
 # 5. make JS code beautiful before parsing 
 
+
+# TODO:
+# 1. delete temp files. (tmpfiles for funcs and ...) 
 
 def get_args():
     # 1. getting path to the file
@@ -25,10 +29,12 @@ def get_args():
 
     return file_path
 
-def init(f):
-    # f is a file handler
+def init(fin, fout):
+    # fin is the file handler of input stream
+    # fout is the file handler of output stram
+
     # writing initial objects to f 
-    if not isinstance(f, io.IOBase):
+    if not isinstance(fin, io.IOBase) or not isinstance(fout, io.IOBase):
         raise ValueError('f must be a file!')
 
     initial_code = (
@@ -40,7 +46,11 @@ def init(f):
         '}'
     )
 
-    print('\n'.join(initial_code), file=f)
+    
+    # beautifying code
+    subprocess.run(f'html5-print -t js -o tmp {fin.name}'.split())
+
+    print('\n'.join(initial_code), file=fout)
      
 
 
@@ -48,13 +58,17 @@ if __name__ == '__main__':
     file_path = get_args()  # path to the file
     file_path_ext = os.path.splitext(file_path)
     new_file_name = file_path_ext[0] + '_test' + file_path_ext[1]  # new file path to write in
+    # input_tmp_file = file_path_ext[0] + '_input_test' + file_path_ext[1]  
+    input_tmp_file = 'tmp'   # original JS code is first beautified and copied in this file
     functions_tmp_file_name = file_path_ext[0] + '_test_temp_funcs' + file_path_ext[1] 
 
     file_in = open(file_path, 'r')   # original file
     file_out = open(new_file_name, 'w')    # new file
-    functions_tmp = open(functions_tmp_file_name, 'w') 
+    functions_tmp = open(functions_tmp_file_name, 'w')
 
-    init(file_out)
+    init(file_in, file_out)
+
+    file_in_tmp = open(input_tmp_file, 'r') 
 
     entry_points = ['document']  # can have other variables
     status = True
@@ -67,7 +81,7 @@ if __name__ == '__main__':
     assigned_func_reg = re.compile(assigned_function_pattern)
     normal_func_reg = re.compile(normal_function_pattern)
 
-    for line in file_in:
+    for line in file_in_tmp:
         # for each line in js code
         # it is assumed JS code is a pretty code
 
@@ -76,12 +90,12 @@ if __name__ == '__main__':
         # if declaration_result:
             # declaration statement
         
-        decalre_res = handle_variable_declaration(line, declare_reg, entry_points, file_in, file_out)
-        func_res = handle_functions(line, assigned_func_reg, normal_func_reg, file_in, file_out, functions_tmp)
+        decalre_res = handle_variable_declaration(line, declare_reg, entry_points, file_in_tmp, file_out)
+        func_res = handle_functions(line, assigned_func_reg, normal_func_reg, file_in_tmp, file_out, functions_tmp)
 
-        if decalre_res:
+        if func_res:
             pass
-        elif func_res:
+        elif decalre_res:
             pass
         else:
             print(line, file=file_out)
@@ -106,11 +120,14 @@ if __name__ == '__main__':
 
     file_in.close()
     file_out.close()
+    file_in_tmp.close()
+    # os.remove(input_tmp_file)
+
     print(entry_points)
 
     print(new_file_name)
 
-    subprocess.run(['../expoSE', new_file_name])
+    # subprocess.run(['../expoSE', new_file_name])
 
 
     
