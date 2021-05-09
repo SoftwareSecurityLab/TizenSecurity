c4714cb1976e16e8c0fec0b8bb1cc6e0cc263d5f
sobhan <sobhan.saf79@gmail.com>
2021-05-09 12:58:12 +0430

    a test file for function calls

---
+++ tests/DOM/test5.js
@@ -0,0 +1,8 @@
+f();
+fun(a, b, c);
+if (foo(10, 12, 15) == 20);
+a = bar(x, y, z) + foo(10);
+k = foo(bar(x));
+if (foo(document.getElementById('theId'), fun(123)));
+fun(fun1(document.getElementById('theId')), abc, fun2(fun3()));
+
